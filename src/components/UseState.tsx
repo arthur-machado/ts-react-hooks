b7ad32a2488ad1e2a66ff40c4fda1b2767b859e5
arthur-machado <arthurmachado7@gmail.com>
2021-07-20 11:07:21 -0300

    Formata imagem.

--- src/components/UseState.tsx
+++ src/components/UseState.tsx
@@ -1,50 +1,54 @@
 import React, { useState } from 'react';
 
 interface UserProps {
-    name: string;
-    login: string;
-    avatar_url: string;
+  name: string;
+  login: string;
+  avatar_url: string;
 }
 
 const UseState: React.FC = () => {
-    const [userData, setUserData] = useState<UserProps>();
-    const [searchQuery, setSearchQuery] = useState('');
+  const [userData, setUserData] = useState<UserProps>();
+  const [searchQuery, setSearchQuery] = useState('');
 
-    async function loadData(userQuery: string) {
-        const response = await fetch(`https://api.github.com/users/${userQuery}`);
-        const data = await response.json();
+  async function loadData(userQuery: string) {
+    const response = await fetch(`https://api.github.com/users/${userQuery}`);
+    const data = await response.json();
 
-        setUserData(data);
-    }
+    setUserData(data);
+  }
 
-    function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-        let query = e.target.value;
-        setSearchQuery(query);
-    }
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    let query = e.target.value;
+    setSearchQuery(query);
+  }
 
-    return (
-        <>
-            <h3>Exemplo com useState</h3>
-            <div>
-                <label>Usuário Github: </label>
-                <input type="text" onChange={handleInputChange} />
-                <button onClick={() => loadData(searchQuery)}>Buscar</button>
-            </div>
+  return (
+    <>
+      <h3>Exemplo com useState</h3>
+      <div>
+        <label>Usuário Github: </label>
+        <input type="text" onChange={handleInputChange} />
+        <button onClick={() => loadData(searchQuery)}>Buscar</button>
+      </div>
 
-            <ul>
-                {userData && (
-                    <>
-                        <li>
-                            <img src={userData?.avatar_url} alt="User avatar" />
-                        </li>
-                        <li>{userData?.name}</li>
-                        <li>{userData?.login}</li>
-                    </>
-                )}
-            </ul>
-            <hr />
-        </>
-    );
+      <ul>
+        {userData && (
+          <>
+            <li style={{ listStyle: 'none' }}>
+              <img
+                src={userData?.avatar_url}
+                alt="User avatar"
+                style={{ width: '100px', height: '100px', borderRadius: '50%' }}
+              />
+            </li>
+            <li>{userData?.name}</li>
+            <li>{userData?.login}</li>
+          </>
+        )}
+      </ul>
+      <hr />
+    </>
+  );
 };
 
 export default UseState;
