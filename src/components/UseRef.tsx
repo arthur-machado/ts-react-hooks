74d6bfb57d10c7fa482d6fc15cb89cd0317c9bc7
arthur-machado <arthurmachado7@gmail.com>
2021-07-20 11:00:58 -0300

    Adiciona o componente useRef.

--- src/components/UseRef.tsx
+++ src/components/UseRef.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 interface UserProps {
   name: string;
@@ -9,6 +9,8 @@ interface UserProps {
 const UseRef: React.FC = () => {
   const [userData, setUserData] = useState<UserProps>();
   const [searchQuery, setSearchQuery] = useState('');
+  // Cria a referência ao elemento Input
+  const inputText = useRef<HTMLInputElement>(null);
 
   async function loadData(userQuery: string) {
     const response = await fetch(`https://api.github.com/users/${userQuery}`);
@@ -21,12 +23,18 @@ const UseRef: React.FC = () => {
     let query = e.target.value;
     setSearchQuery(query);
   }
+
+  useEffect(() => {
+    //Seta o focus ao Input quando o componente renderizar
+    inputText.current?.focus();
+  });
+
   return (
     <>
       <h3>Exemplo com useRef</h3>
       <div>
         <label>Usuário Github: </label>
-        <input type="text" onChange={handleInputChange} />
+        <input type="text" onChange={handleInputChange} ref={inputText} />
         <button onClick={() => loadData(searchQuery)}>Buscar</button>
       </div>
 
