1f1f136b308aa54f111a6b1af01feff81b245538
arthur-machado <arthurmachado7@gmail.com>
2021-07-20 15:35:26 -0300

    Adiciona contexto de tema.

--- src/components/UseContext.tsx
+++ src/components/UseContext.tsx
@@ -1,11 +1,17 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../context/UserContext';
-
+import { ThemeContext } from '../context/ThemeContext';
 const UseContext: React.FC = () => {
   const { name, email } = useContext(UserContext);
+  const context = useContext(ThemeContext);
 
   return (
-    <>
+    <div
+      style={{
+        backgroundColor: `${context?.theme.background}`,
+        color: `${context?.theme.text}`,
+      }}
+    >
       <h3>Exemplo com useContext</h3>
       <ul>
         <li>{name}</li>
@@ -13,7 +19,7 @@ const UseContext: React.FC = () => {
         <span>Variáveis definidas dentro do contexto</span>
       </ul>
       <hr />
-    </>
+    </div>
   );
 };
 
