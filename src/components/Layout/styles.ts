9aa72398b506c2f247f1b790370dd6a544f0af27
arthur-machado <arthurmachado7@gmail.com>
2021-07-21 14:02:31 -0300

    Finaliza estilização.

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -9,16 +9,43 @@ export const Container = styled.section<StylesProps>`
   height: 100vh;
   width: 100%;
 
-  padding: 2rem 0;
-
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-evenly;
+  justify-content: center;
 
   background-color: ${(props) => props.theme.background};
   color: ${(props) => props.theme.textColor};
   font-family: 'Roboto', sans-serif;
+
+  > div {
+    margin: 8px 0;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  > div > button {
+    margin: 10px 5px;
+    background-color: ${(props) => props.theme.blue};
+    color: #fff;
+    padding: 10px;
+    border-radius: 5px;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    width: 120px;
+  }
+
+  > div > hr {
+    margin: 12px 0;
+    width: 100%;
+  }
+
+  > div > h3 {
+    margin-bottom: 10px;
+  }
 `;
 
 export const Button = styled.button<StylesProps>`
